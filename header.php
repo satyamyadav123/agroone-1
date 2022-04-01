dbe8c049ace9c73173465cfe6f8e9b0848d666d9
Arpit <arpit.porwal@webhungers.com>
2022-04-01 10:50:12 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="keywords" content="HTML5 Template" />
+<meta name="description" content="agroone – Agriculture HTML Template" />
+<meta name="author" content="https://www.themetechmount.com/" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2" />
+<title>Agroone – Agriculture</title>
+
+<!-- favicon icon -->
+<link rel="shortcut icon" href="images/favicon.png" />
+
+<!-- bootstrap -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+
+<!-- animate -->
+<link rel="stylesheet" type="text/css" href="css/animate.css"/>
+
+<!-- slick-slider -->
+<link rel="stylesheet" type="text/css" href="css/slick.css">
+
+<link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+
+<!-- fontawesome -->
+<link rel="stylesheet" type="text/css" href="css/font-awesome.css"/>
+
+<!-- themify -->
+<link rel="stylesheet" type="text/css" href="css/themify-icons.css"/>
+
+<!-- flaticon -->
+<link rel="stylesheet" type="text/css" href="css/flaticon.css"/>
+
+
+<!-- REVOLUTION LAYERS STYLES -->
+
+    <link rel="stylesheet" type="text/css" href="revolution/css/rs6.css">
+
+<!-- prettyphoto -->
+<link rel="stylesheet" type="text/css" href="css/prettyPhoto.css">
+
+<!-- shortcodes -->
+<link rel="stylesheet" type="text/css" href="css/shortcodes.css"/>
+
+<!-- main -->
+<link rel="stylesheet" type="text/css" href="css/main.css"/>
+
+<!-- responsive -->
+<link rel="stylesheet" type="text/css" href="css/responsive.css"/>
+
+</head>
+<style type="text/css">
+    .kc_row.kc_row_inner {
+      width: calc(100% + 30px);
+    }
+    .kc-pie-chart-wrapper, .kc_counter_box {
+      text-align: center;
+    }
+    .kc_piechart {
+        display: inline-block;
+        position: relative;
+        text-align: center;
+        width: 100px;
+   }
+   .kc_piechart .pie_chart_percent {
+    z-index: 2;
+    top: 50%;
+    position: absolute;
+    left: 50%;
+    text-align: center;
+    font-size: 14px;
+    transform: translateX(-50%) translateY(-50%);
+    -webkit-transform: translateX(-50%) translateY(-50%);
+   }
+
+   .kc_piechart .percent {
+    z-index: 2;
+    text-align: center;
+    font-size: 14px;
+    display: block;
+    color: #42BCE2;
+   }
+   .kc_piechart canvas {
+    left: 0;
+    position: absolute;
+    top: 0; 
+   }
+   .kc_column, .kc_column_inner {
+    min-height: 1px;
+    position: relative;
+    padding-right: 15px;
+    padding-left: 15px;
+    float: left;
+  }
+  .kc-pie-chart-wrapper, .kc_counter_box {
+    text-align: center;
+  }
+  /*.kc-elm {
+    float: left;
+    width: 100%;
+  }*/
+  /*.kc_text_block {
+    display: inline-block;
+    clear: both;
+    width: 100%;
+  }*/
+  .kc_piechart .percent:after {
+    content: "%";
+    font-size: .8em;
+    margin-left: .1em;
+  }
+  .kc_piechart canvas {
+    left: 0;
+    position: absolute;
+    top: 0;
+  }
+</style>
+<body>
+
+    <!--page start-->
+    <div class="page">
+
+        <!-- preloader start -->
+        <div id="preloader">
+          <div id="status">&nbsp;</div>
+        </div>
+        <!-- preloader end -->
+
+        <!--header start-->
+        <header id="masthead" class="header ttm-header-style-02">
+            <!-- ttm-topbar-wrapper -->
+            <div class="ttm-topbar-wrapper ttm-bgcolor-darkgrey ttm-textcolor-white clearfix" style="">
+                <div class="container">
+                    <div class="ttm-topbar-content">
+                        <ul class="top-contact text-left">
+                            <li class="list-inline-item"><span class="ttm-textcolor-skincolor"><i class="fa fa-user"></i>  24/7 Emergency Services Provide </span> :+91 7224094409</li>
+                        </ul>
+                        <div class="topbar-right text-right">
+                            <ul class="top-contact ttm-highlight-right">
+                                <li><span class="ttm-textcolor-skincolor">Send Email</span> : agrooneindore@gmail.com</li>
+                            </ul>
+                            <div class="ttm-social-links-wrapper list-inline">
+                                <ul class="social-icons">
+                                    <li><a href="https://www.facebook.com/Agroone-105945794778196" rel="noopener" aria-label="facebook"><i class="fa fa-facebook"></i></a>
+                                    </li>
+                                    <li><a href="https://www.instagram.com/agro.one.indore/" rel="noopener" aria-label="instagram"><i class="fa fa-instagram"></i></a>
+                                    </li>
+                                    <li><a href="https://www.linkedin.com/company/themetech-mount/" rel="noopener" aria-label="twitter"><i class="fa fa-youtube"></i></a>
+                                    </li>
+                                    <!-- <li><a href="https://in.pinterest.com/themetechmount/_created/" rel="noopener" aria-label="pinterest-p"><i class="fa fa-pinterest-p"></i></a>
+                                    </li> -->                                    
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- ttm-topbar-wrapper end -->
+            <!-- ttm-header-wrap -->
+            <div class="ttm-header-wrap"  style="background-color: #ffff;">
+                <!-- ttm-stickable-header-w -->
+                <div id="ttm-stickable-header-w" class="ttm-stickable-header-w clearfix">
+                    <div id="site-header-menu" class="site-header-menu">
+                        <div class="site-header-menu-inner ttm-stickable-header">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="site-navigation d-xl-flex flex-row align-items-center justify-content-between">
+                                            <!-- site-branding -->
+                                            <div class="site-branding">
+                                                <a class="home-link" href="index.html" title="Agrotek" rel="home">
+                                                    <img width="211" height="54" id="logo-img" class="img-center" src="images/Agro_one.png" alt="logo-img">
+                                                </a>
+                                            </div><!-- site-branding end -->
+                                            <!--site-navigation -->
+                                            <div id="site-navigation" class="site-navigation">
+                                                <!-- header-icons -->
+                                                <div class="ttm-header-icons">
+                                                    <a href="contact.html" class="ttm-btn ttm-btn-size-md ttm-btn-shape-rounded ttm-btn-style-border ttm-icon-btn-right ttm-btn-color-darkgrey">Get Quoted</a>
+                                                </div><!-- header-icons end -->
+                                                <div class="ttm-menu-toggle">
+                                                    <input type="checkbox" id="menu-toggle-form" />
+                                                    <label for="menu-toggle-form" class="ttm-menu-toggle-block">
+                                                        <span class="toggle-block toggle-blocks-1"></span>
+                                                        <span class="toggle-block toggle-blocks-2"></span>
+                                                        <span class="toggle-block toggle-blocks-3"></span>
+                                                    </label>
+                                                </div>
+                                                <nav id="menu" class="menu">
+                                                    <ul class="dropdown">
+                                                       <li class="active"><a href="index.html">Home</a></li>
+                                                        <li><a href="aboutus-01.html">About Us</a>
+                                                            <!-- <ul>
+                                                                <li><a href="aboutus-01.html">About Us 1</a></li>
+                                                                <li><a href="aboutus-02.html">About Us 2</a></li>
+                                                                <li><a href="services-01.html">Services 1</a></li>
+                                                                <li><a href="services-02.html">Services 2</a></li>
+                                                                <li><a href="contact.html">Contact Us</a></li>
+                                                                <li><a href="our-expert.html">Our Team</a></li>
+                                                                <li><a href="faq.html">FAQs</a></li>
+                                                                <li><a href="error.html">Error Page</a></li>
+                                                                <li><a href="element.html">Elements</a></li>
+                                                            </ul> -->
+                                                        </li>
+                                                        <li><a href="services-02.html">Services</a>
+                                                            <!-- <ul>
+                                                                <li><a href="lawn-garden-care.html">Lawn & Garden Care</a></li>
+                                                                <li><a href="stone-hardscaping.html">Stone & Hardscaping</a></li>
+                                                                <li><a href="forest-tree-planting.html">Forest & Tree Planting</a></li>
+                                                                <li><a href="watering-systems.html">Watering Systems</a></li>
+                                                                <li><a href="preparing-landscape.html">Preparing Landscape</a></li>
+                                                                <li><a href="irrigation-drainage.html">Irrigation & Drainage</a></li>
+                                                            </ul> -->
+                                                        </li>
+                                                        <li><a href="lawn-garden-care.html">Product</a>
+                                                           <!--  <ul>
+                                                                <li><a href="project-style-01.html">Project Style 1</a></li>
+                                                                <li><a href="project-style-02.html">Project Style 2</a></li>
+                                                                <li><a href="#">Project Details</a>
+                                                                    <ul>
+                                                                        <li><a href="detail-style-01.html">Detail Style 1</a></li>
+                                                                        <li><a href="detail-style-02.html">Detail Style 2</a></li>
+                                                                        <li><a href="detail-style-03.html">Detail Style 3</a></li>
+                                                                    </ul>
+                                                                </li>
+                                                            </ul> -->
+                                                        </li>
+                                                      <!--   <li><a href="#">Blog</a>
+                                                            <ul>
+                                                                <li><a href="blog.html">Blog Classic</a></li>
+                                                                <li><a href="blog-grid.html">Blog Grid View</a></li>
+                                                                <li><a href="blog-top-image.html">Blog Top Image</a></li>
+                                                                <li><a href="blog-left-image.html">Blog Left Image</a></li>
+                                                                <li><a href="single-blog.html">Blog Single View</a></li>
+                                                            </ul>
+                                                        </li>
+                                                        <li><a href="#">Shop</a>
+                                                            <ul>
+                                                                <li><a href="shop.html">Default Shop</a></li>
+                                                                <li><a href="product-details.html">Single Product Details</a></li>
+                                                                <li><a href="cart.html">Cart</a></li>
+                                                                <li><a href="checkout.html">Checkout</a></li>
+                                                            </ul>
+                                                        </li> -->
+                                                    </ul>
+                                                </nav>
+                                            </div><!-- site-navigation end-->
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- ttm-stickable-header-w end-->
+            </div><!--ttm-header-wrap end -->
+        </header><!--header end-->
